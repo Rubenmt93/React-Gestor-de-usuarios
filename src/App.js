0906d25e0210fbc539f87f580c371d40d1def25c
Ruben Montes <rubenmontes330@gmail.com>
2023-02-03 12:00:52 +0000

    Container y burtron components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import useFormulario from './hooks/useFormulario'
 import Input from './components/Input'
 import Card from './components/Card'
-
+import Container from './components/Container'
+import Button from  './components/Button'
 function App() {
 
-  const [formulario, handleChange] = useFormulario({name:'',lastname:''})
- 
-  // const[formulario, setFormulario]= useState({name: ''})
-  //   const handleChange= (e) => {
-  //       setFormulario({
-  //                   ...formulario, 
-  //                   [e.target.name]: e.target.value
-  //       })
-  //   }
-
-    console.log(formulario);
+  const [formulario, handleChange] = useFormulario({
+                                                      name:'',
+                                                      lastname:'',
+                                                      email:''
+                                                    })
+  console.log(formulario);
   return (
-    <Card>
-      <form>
-        <Input  label="Nombre" 
-                name="name" 
-                value={formulario.name} 
-                onChange={handleChange}/>
+    <Container>
+      <Card>
+        <div style={{padding:20}}>
+          <form>
+            <Input  label="Nombre" 
+                    name="name" 
+                    value={formulario.name} 
+                    onChange={handleChange}/>
 
-        <Input  label="Apellido" 
-                name="lastname" 
-                value={formulario.lastname} 
-                onChange={handleChange}/>
-    
-      </form>
-    </Card>
+            <Input  label="Apellido" 
+                    name="lastname" 
+                    value={formulario.lastname} 
+                    onChange={handleChange}/>
+
+            <Input  label="Correo" 
+                    name="email" 
+                    value={formulario.email} 
+                    onChange={handleChange}/>
+            <Button>Enviar</Button>
+          </form>
+        </div>
+       
+      </Card>
+    </Container>
+   
     
   );
 }
