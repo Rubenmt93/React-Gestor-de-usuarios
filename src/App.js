df4bc1eb333fbd6f9f5c0dc182e6822bf26e2ba2
Ruben Montes <rubenmontes330@gmail.com>
2023-02-04 13:07:45 +0000

    mostrar informacion de los usuarios

--- src/App.js
+++ src/App.js
@@ -3,19 +3,29 @@ import Input from './components/Input'
 import Card from './components/Card'
 import Container from './components/Container'
 import Button from  './components/Button'
+import { useState } from 'react'
 function App() {
-
-  const [formulario, handleChange] = useFormulario({
+  const [usuarios, setUsuarios]= useState([])
+  const [formulario, handleChange, reset] = useFormulario({
                                                       name:'',
                                                       lastname:'',
                                                       email:''
                                                     })
-  console.log(formulario);
+
+  const submit = e => {
+    e.preventDefault()
+    setUsuarios([
+      ...usuarios,
+      formulario,
+    ])
+    reset()
+  }
+ 
   return (
     <Container>
       <Card>
         <div style={{padding:20}}>
-          <form>
+          <form onSubmit={submit}>
             <Input  label="Nombre" 
                     name="name" 
                     value={formulario.name} 
@@ -34,6 +44,12 @@ function App() {
           </form>
         </div>
        
+      </Card>
+      <Card>
+        <ul>
+          {usuarios.map(usuario => 
+            <li key={usuario.email}>{`${usuario.name} ${usuario.lastname}: ${usuario.email}`}</li>)}
+        </ul>
       </Card>
     </Container>
    
--- src/hooks/useFormulario.js
+++ src/hooks/useFormulario.js
@@ -8,6 +8,9 @@ const useFormulario = (inicial) => {
                     [e.target.name]: e.target.value
         })
     }
-    return [formulario,handleChange]
+    const reset = () =>{
+        setFormulario(inicial)
+    }
+    return [formulario,handleChange,reset]
 }
 export default useFormulario
